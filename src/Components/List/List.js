a3ddbcee481e3fd0d776136f0d870f23f50a0aca
Tega Oke <oketegah@gmail.com>
2019-06-25 00:33:07 +0100

    Added function for true edit state and added onclick event handler

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -5,16 +5,30 @@ import './List.css';
 function List() {
   const [editState, setEditState] = useState({ editing: false });
 
+  const edit = () => {
+    setEditState({ editing: true });
+  };
+
   if (!editState.editing) {
     return (
       <div className="single-list">
         <span className="contact-name">tega</span>
         <span className="contact-number">08051812473</span>
-        <button>
+        <button onClick={edit}>
           <i className="mdi mdi-account-edit" />
         </button>
         <button>
           <i className="mdi mdi-delete" />
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="single-list">
+        <input type="text" />
+        <input type="number" />
+        <button>
+          <i className="mdi mdi-content-save-edit" />
         </button>
       </div>
     );
