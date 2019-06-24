95994f63ad88ec08927407a3dbc551ef5fcb2b84
Tega Oke <oketegah@gmail.com>
2019-06-24 23:58:56 +0100

    Adde Button and remove background image

---
+++ src/Components/ListBoard/ListBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ListBoard.css';
+
+function ListBoard() {
+  return (
+    <div className="display">
+      <button className="add-new-button">
+        <i className="mdi mdi-plus" />
+      </button>
+    </div>
+  );
+}
+
+export default ListBoard;
